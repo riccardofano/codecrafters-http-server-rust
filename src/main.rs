1fc9755629722a8edd5ea365f6e15211e28aafbb
Riccardo Fano <rcr.fano@gmail.com>
2024-07-13 09:44:04 +0200

    Make server multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,72 @@
-// Uncomment this block to pass the first stage
 use std::{
     collections::HashMap,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        Arc, Mutex,
+    },
+    thread,
 };
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Sender<Job>,
+}
+
+impl ThreadPool {
+    fn new(size: usize) -> Self {
+        let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for i in 0..size {
+            workers.push(Worker::new(i, receiver.clone()))
+        }
+
+        Self { workers, sender }
+    }
+
+    fn execute<F: FnOnce() + Send + 'static>(&self, function: F) {
+        let job = Box::new(function);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    handle: std::thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Self {
+        let handle = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
+        Self { id, handle }
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let pool = ThreadPool::new(10);
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                handle_connection(stream);
+
+                pool.execute(|| {
+                    handle_connection(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -39,15 +88,17 @@ fn handle_connection(mut stream: TcpStream) {
         .try_into()
         .unwrap();
 
-    let (headers, _body) = rest.split_once("\r\n\r\n").unwrap();
-    let headers = headers
-        .split("\r\n")
-        .map(|line| {
-            line.split_once(": ")
-                .map(|(h, c)| (h.to_lowercase(), c))
-                .unwrap()
-        })
-        .collect::<HashMap<_, _>>();
+    let mut headers: HashMap<String, &str> = HashMap::new();
+    if let Some((string_headers, _body)) = rest.split_once("\r\n\r\n") {
+        headers = string_headers
+            .split("\r\n")
+            .map(|line| {
+                line.split_once(": ")
+                    .map(|(h, c)| (h.to_lowercase(), c))
+                    .unwrap()
+            })
+            .collect::<HashMap<_, _>>();
+    }
 
     let response = match path {
         "/" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
